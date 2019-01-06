c1d3db602f18b28e68ffbf5bab07c9bb3e0e3589
AllenB23 <46408750+Byen23@users.noreply.github.com>
2019-01-06 08:21:30 -0800

    Inputs-two-int-outputs-value
    Both in spyder IDE, and Jupyter Notebooks

---
+++ 3.Inputs-two-integers-and-displays-their-value.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  6 07:52:35 2019
+
+@author: Byen23
+"""
+#This is the third program that will be uploaded to github.
+'''Input two integers and displays their sum, then subtraction,
+    division, and multiplication.'''
+    
+print("\n"+62*"*")
+num1 = int(input("Enter first num: "))
+num2 = int(input("Enter second num: "))
+
+print(num1, "+", num2, "=", num1 + num2)
+print("\n"+62*"*")
+
+num1 = int(input("Enter first num: "))
+num2 = int(input("Enter second num: "))
+
+print(num1, "-", num2, "=", num1 - num2)
+print("\n"+62*"*")
+
+num1 = int(input("Enter first num: "))
+num2 = int(input("Enter second num: "))
+
+print(num1,"/", num2,"=", num1 / num2)
+print("\n"+62*"*")
+
+num1 = int(input("Enter first num: "))
+num2 = int(input("Enter second num: "))
+
+print(num1,"*", num2, "=", num1 * num2)
+
+
